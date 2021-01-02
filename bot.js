770ccdd03a704dbc02a18cc3564ac4b5fa6eae50
RootSec <itznaktuminajasiftw@gmail.com>
2021-01-02 14:37:39 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const { TeamSpeak, QueryProtocol } = require("ts3-nodejs-library");
-const { Commander } = require("teamspeak-commander");
 const { get } = require("snekfetch");
 
 //Podesavanja
