a9dc7a6e55f6dc73532e93b808839e472198e8f7
ＨＡＺＺＥ <60288878+hazzeee@users.noreply.github.com>
2021-03-24 21:43:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,12 +9,12 @@ const htmlparser2 = require("htmlparser2");
 
 
 //Podesavanja
-const host                = "5.254.118.212";
-const port                = 1312;
+const host                = "ts3.elitegaming.me";
+const port                = 9987;
 const queryport           = 10011;
 const username            = "serveradmin";
-const password            = "leiCZAwP";
-const botname             = "onomoe";
+const password            = "2k5cvhRtc6pSDk";
+const botname             = "UGB.RS BOT";
 
 const debug               = false;  //DEBUG LOG
 const error               = false;  //ERROR LOG
